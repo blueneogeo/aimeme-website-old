2c63e55d85a143cb224f0c4cfb58c2d75ac0532c
Christian Vogel <chris.vogel@me.com>
2019-06-12 13:46:27 +0200

    added admin view of users that uses the nestjs server api

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,6 +11,17 @@ module.exports = {
 				pug: { /* Options for `pug-plain-loader` */ },
 				pugLoader: { /* Options for `pug-loader` */ }
 			}
-		}
+    },
+    {
+      use: '@gridsome/source-graphql',
+      options: {
+        url: 'http://localhost:3001/graphql',
+        fieldName: 'aimeme',
+        typeName: '',
+        headers: {
+          // Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+        },
+      },
+    }
   ],
 }
---
+++ src/pages/Admins.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  layout
+    g-image.image(alt='Gridsome' src='~/assets/favicon.png' width=135)
+
+    h1.title {{ title }}
+
+    section#users
+      p
+        | Here are all the currently registered users:
+      div(v-for="user in $page.aimeme.users" :key="user.id")
+        h3 
+          a(href='#' @click='onClickDeleteUser(user.id)')
+            | {{user.email}} : {{user.role}} - pw: {{user.password}}
+
+    p.home-links
+      a(href="https://gridsome.org/docs" target="_blank" rel="noopener") Gridsome Docs
+      a(href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener") GitHub
+
+</template>
+
+<page-query>
+  query {
+    aimeme {
+      users {
+        email,
+        password
+        role
+      }
+    }
+  }
+</page-query>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class UsersPage extends Vue {
+    title = 'All AImeme users'
+
+    onClickDeleteUser(id: number) {
+      console.log((this as any).$apollo)
+    }
+
+  }
+</script>
